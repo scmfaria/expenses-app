98adc75213c263dede14e038eb8fcfdd9fe67103
scmfaria <sara.moura.faria@gmail.com>
2020-11-26 22:39:18 -0300

    add ChartBar

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,6 +1,9 @@
+import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import 'chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -37,17 +40,34 @@ class Chart extends StatelessWidget {
     });
   }
 
+  // retorna a soma total da semana 
+  double get _weekTotalValue {
+    // fold funciona bem parecido com o reduce
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + tr['value'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactions.map((tr) {
-          return Text(
-            '${tr['day']}: ${tr['value']}'
-          );
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'],
+                value: tr['value'],
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
+  final String label;
+  final double value;
+  final double percentage;
+
+  // quando coloca {} é porque os parametros serao nomeados
+  ChartBar({
+    this.label, 
+    this.value, 
+    this.percentage
+  });
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: [
+        // toStringAsFixed -> é para ter no caso 2 numeros depois da virgula
+        FittedBox(
+          child: Text('${value.toStringAsFixed(2)}'),
+        ),
+        SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          // widget da barrinha que tem a quantidade pintada
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5), // para ficar com as bordas redondidas
+                ),
+              ),
+              // mesma coisa que SizedBox, so que esse é fracionado 
+              FractionallySizedBox(
+                heightFactor: percentage, // o fator que ele vai usar para pintar o componente é a porcentagem 
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor, // vai pintar da cor primaria
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label),
+      ],
     );
   }
 }
