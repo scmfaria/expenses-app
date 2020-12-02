5f060a3b2f6daa77a0cd5b945408f772b1e5153b
scmfaria <sara.moura.faria@gmail.com>
2020-12-02 16:01:55 -0300

    add DatePicker in forms

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
 
@@ -13,6 +14,7 @@ class TransactionForm extends StatefulWidget {
 class _TransactionFormState extends State<TransactionForm> {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+  DateTime _selectedDate;
 
   _submitForm() {
     // nesse caso, o componente pai (transaction_user) passou para o componente filho (transaction_form) uma funcao
@@ -27,6 +29,24 @@ class _TransactionFormState extends State<TransactionForm> {
     }
 
     widget.onSubmit(title, value);
+  }
+
+  _showDatePicker() {
+    // esse metodo retorna um Future<DateTime>
+    showDatePicker(
+      context: context, 
+      initialDate: DateTime.now(), 
+      firstDate: DateTime(2019), 
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if(pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -53,15 +73,40 @@ class _TransactionFormState extends State<TransactionForm> {
                   labelText: 'Valor (R\$)',
                 ),
               ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null ? 
+                        'Nenhuma data selecionada!' : 
+                        'Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}'
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: _showDatePicker, 
+                      child: Text(
+                        'Selecionar Data',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  ],
+                ),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  FlatButton(
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
                     child: Text('Nova Transação'),
                     onPressed: () {
                       _submitForm();   
                     },
-                    textColor: Colors.purple,
+                    textColor: Colors.white,
                   ),
                 ],
               )
--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,10 @@ class ExpensesApp extends StatelessWidget {
               fontSize: 18,
               fontWeight: FontWeight.bold,
             ),
+            button: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
